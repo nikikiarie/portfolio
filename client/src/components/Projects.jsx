a52230252a4e5a27ba1c19ab544ecd787ecf3574
nikikiarie <110396228+nikikiarie@users.noreply.github.com>
2023-04-12 17:46:15 +0300

    links commit

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import mockup from "../assets/mockup.png";
+import React, { useState } from "react";
 import mockup2 from "../assets/mockup2.png";
 import mockup3 from "../assets/mockup3.png";
 import { AiFillGithub } from "react-icons/ai";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Projects = () => {
+  const [showMessage, setShowMessage] = useState(false);
+  const [showMessageTwo, setShowMessageTwo] = useState(false);
+
   return (
     <section className="px-10 mt-32 md:mt-0 md:px-24 py-10 my-10">
       <div className="w-full text-white flex justify-center">
@@ -21,30 +24,90 @@ const Projects = () => {
           <div className="flex flex-col md:grid grid-cols-2 gap-5 ">
             <div className="flex flex-col items-center justify-center">
               <div className="mb-5">
-                <div className="flex justify-center text-3xl">
-                  <a href="https://github.com/nikikiarie/socia">
-                    <AiFillGithub />
-                  </a>
-                </div>
+                <div className="flex justify-center text-3xl"></div>
                 <h2 className="text-lg text-gray-400">E-commerce</h2>
-
-                <div></div>
               </div>
-              <div>
+              <div
+                className="relative hover:shadow-inner"
+                onMouseEnter={() => {
+                  setShowMessage(true);
+                }}
+                onMouseLeave={() => {
+                  setShowMessage(false);
+                }}
+              >
                 <img src={mockup3} alt="hfgj" className="h-[250px]" />
+                {showMessage && (
+                  <div className="absolute flex items-center justify-center w-full h-full top-0 left-0">
+                    <div className="bg-black bg-opacity-50 w-full h-full flex flex-col items-center justify-center">
+                      <a
+                        href="https://maroon-crayfish-fez.cyclic.app"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h1 className="text-[#27AE60] text-lg flex items-center underline font-extrabold">
+                          Live
+                          <span>
+                            <FiArrowUpRight />
+                          </span>
+                        </h1>
+                      </a>
+                      <a
+                        href="https://github.com/nikikiarie/socia"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <span className="text-2xl">
+                          <AiFillGithub />
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
             <div className="flex flex-col items-center justify-center mt-5 md:mt-0">
               <div className="mb-5">
-                <div className="flex justify-center text-3xl">
-                  <a href="https://github.com/nikikiarie/mern_commerce_cyclic">
-                    <AiFillGithub />
-                  </a>
-                </div>
+                <div className="flex justify-center text-3xl"></div>
                 <h2 className="text-lg text-gray-400">Social Media</h2>
               </div>
-              <div>
+              <div
+                className="relative hover:shadow-inner"
+                onMouseEnter={() => {
+                  setShowMessageTwo(true);
+                }}
+                onMouseLeave={() => {
+                  setShowMessageTwo(false);
+                }}
+              >
                 <img src={mockup2} alt="hfgj" className="h-[250px]" />
+                {showMessageTwo && (
+                  <div className="absolute flex items-center justify-center w-full h-full top-0 left-0">
+                    <div className="bg-black bg-opacity-50 w-full h-full flex flex-col items-center justify-center">
+                      <a
+                        href="https://fair-ruby-lemming-kilt.cyclic.app"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <h1 className="text-[#27AE60] text-lg flex items-center underline font-extrabold">
+                          Live
+                          <span>
+                            <FiArrowUpRight />
+                          </span>
+                        </h1>
+                      </a>
+                      <a
+                        href="https://github.com/nikikiarie/mern_commerce_cyclic"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <span className="text-2xl">
+                          <AiFillGithub />
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
